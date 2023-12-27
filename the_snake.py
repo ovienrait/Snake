b79788cadf20b7e40cd4058d28a123c8e136661e
Artem Maksimov <nirendsound@gmail.com>
2023-12-27 17:09:13 +0300

    Исправлено

--- the_snake.py
+++ the_snake.py
@@ -10,6 +10,7 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+SCREEN_CENTER = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
 # Направления движения
 UP = (0, -1)
@@ -22,11 +23,15 @@ BOARD_BACKGROUND_COLOR = (69, 71, 54)
 APPLE_COLOR = (228, 138, 70)
 MUSHROOM_1_COLOR = (143, 82, 152)
 MUSHROOM_2_COLOR = (41, 108, 149)
+SNAKE_COLOR = (56, 142, 60)
 BLACK_COLOR = (0, 0, 0)
 WHITE_COLOR = (255, 255, 255)
 
 # Скорость движения змейки
 SPEED = 15
+
+# Путь к фоновому изображению
+BACKGROUND_IMAGE = './images/PixelBackground.png'
 
 # Настройка игрового окна
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -42,9 +47,9 @@ clock = pygame.time.Clock()
 class GameObject:
     """Базовый класс игры"""
 
-    def __init__(self, position_x=None, position_y=None, body_color=None):
+    def __init__(self, body_color=None, position=SCREEN_CENTER):
         """Конструктор базового класса"""
-        self.position = (position_x, position_y)
+        self.position = position
         self.body_color = body_color
 
     def draw(self):
@@ -52,64 +57,52 @@ class GameObject:
         pass
 
 
-class Apple(GameObject):
-    """Дочерний класс для экземпляров яблока"""
+class Food(GameObject):
+    """Дочерний класс для экземпляров еды"""
 
-    def __init__(self, body_color=None):
-        """Конструктор дочернего класса для экземпляра яблока"""
-        self.body_color = body_color
-        self.position = self.randomize_position()
+    ICON = ''
 
     def randomize_position(self):
-        """Метод определения случайного положения яблока"""
+        """Метод определения случайного положения еды"""
         position_x = randint(0, GRID_WIDTH - 1)
         position_y = randint(0, GRID_HEIGHT - 1)
-        return (position_x * GRID_SIZE), (position_y * GRID_SIZE)
+        self.position = (position_x * GRID_SIZE, position_y * GRID_SIZE)
 
     def draw(self, surface):
-        """Метод для отрисовывания яблока на экране"""
-        x_axis = self.position[0]
-        y_axis = self.position[1]
+        """Метод для отрисовывания еды на экране"""
+        x_axis, y_axis = self.position
         rect = (
             pygame.Rect((x_axis, y_axis), (GRID_SIZE, GRID_SIZE))
         )
         pygame.draw.rect(surface, self.body_color, rect, 0, 4)
-        apple = pygame.image.load('./images/PixelApple.png')
-        apple.set_colorkey((0, 0, 0))
-        surface.blit(apple, (x_axis + GRID_SIZE / 20, y_axis + GRID_SIZE / 20))
-        pygame.draw.rect(surface, (255, 255, 255), rect, 1, 4)
+        food = pygame.image.load(self.ICON)
+        food.set_colorkey(BLACK_COLOR)
+        surface.blit(food, (x_axis + GRID_SIZE / 20, y_axis + GRID_SIZE / 20))
+        pygame.draw.rect(surface, WHITE_COLOR, rect, 1, 4)
 
 
-class Mushroom(Apple):
+class Apple(Food):
+    """Дочерний класс для экземпляров яблока"""
+
+    ICON = './images/PixelApple.png'
+
+
+class Mushroom(Food):
     """Дочерний класс для экземпляров гриба"""
 
-    def draw(self, surface):
-        """Метод для отрисовывания яблока на экране"""
-        x_axis = self.position[0]
-        y_axis = self.position[1]
-        rect = (
-            pygame.Rect((x_axis, y_axis), (GRID_SIZE, GRID_SIZE))
-        )
-        pygame.draw.rect(surface, self.body_color, rect, 0, 4)
-        mushroom = pygame.image.load('./images/PixelMushroom.png')
-        mushroom.set_colorkey(BLACK_COLOR)
-        surface.blit(mushroom,
-                     (x_axis + GRID_SIZE / 20, y_axis + GRID_SIZE / 20)
-                     )
-        pygame.draw.rect(surface, WHITE_COLOR, rect, 1, 4)
+    ICON = './images/PixelMushroom.png'
 
 
 class Snake(GameObject):
     """Дочерний класс для экземпляров змейки"""
 
-    def __init__(self):
+    def __init__(self, body_color=None, position=SCREEN_CENTER):
         """Конструктор дочернего класса для экземпляра змейки"""
+        super().__init__(body_color, position)
         self.length = 1
-        self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.positions = [self.position]
         self.direction = RIGHT
         self.next_direction = None
-        self.body_color = (56, 142, 60)
 
     def update_direction(self):
         """Метод для обновления направления движения змейки"""
@@ -183,39 +176,90 @@ def handle_keys(game_object):
                 game_object.next_direction = RIGHT
 
 
-def main():  # noqa: max-complexity=13
+def start_position(snake, apple, mushroom_1, mushroom_2):
+    """Функция определения начальных координат объектов"""
+    position_list = []
+    while snake.position == apple.position:
+        apple.randomize_position()
+    position_list.append(snake.position)
+    position_list.append(apple.position)
+    while mushroom_1.position in position_list:
+        mushroom_1.randomize_position()
+    position_list.append(mushroom_1.position)
+    while mushroom_2.position in position_list:
+        mushroom_2.randomize_position()
+    position_list.append(mushroom_2.position)
+    position_list.pop(0)
+    return position_list
+
+
+def speed_tweaking(length, speed):
+    """Функция изменения скорости змейки"""
+    if length < 15:
+        a = 0
+    elif 15 <= length < 30:
+        a = 5
+    elif 30 <= length < 50:
+        a = 10
+    else:
+        a = 15
+    clock.tick(speed + a)
+
+
+def crossing_apple(snake, apple, occupied_positions):
+    """Функция обработки события столкновения с яблоком"""
+    if snake.positions[0] == apple.position:
+        occupied_positions.remove(apple.position)
+        while apple.position in occupied_positions + snake.positions:
+            apple.randomize_position()
+        occupied_positions.insert(0, apple.position)
+        snake.length += 1
+
+
+def crossing_mushroom_1(snake, mushroom_1, occupied_positions):
+    """Функция обработки события столкновения с mushroom_1"""
+    if snake.positions[0] == mushroom_1.position:
+        occupied_positions.remove(mushroom_1.position)
+        while mushroom_1.position in occupied_positions + snake.positions:
+            mushroom_1.randomize_position()
+        occupied_positions.insert(1, mushroom_1.position)
+        if snake.length > 1:
+            snake.length -= 1
+            snake.positions.pop()
+
+
+def crossing_mushroom_2(snake, mushroom_2, occupied_positions):
+    """Функция обработки события столкновения с mushroom_2"""
+    if snake.positions[0] == mushroom_2.position:
+        occupied_positions.remove(mushroom_2.position)
+        while mushroom_2.position in occupied_positions + snake.positions:
+            mushroom_2.randomize_position()
+        occupied_positions.insert(1, mushroom_2.position)
+        if 1 < snake.length < 3:
+            snake.length -= 1
+            snake.positions.pop()
+        elif snake.length >= 3:
+            snake.length -= 2
+            snake.positions.pop()
+            snake.positions.pop()
+
+
+def main():
     """Основной игровой цикл"""
     # Создание экземпляров классов
-    snake = Snake()
+    snake = Snake(SNAKE_COLOR)
     apple = Apple(APPLE_COLOR)
     mushroom_1 = Mushroom(MUSHROOM_1_COLOR)
     mushroom_2 = Mushroom(MUSHROOM_2_COLOR)
-    pos_1, pos_2 = snake.positions[0], apple.position
-    pos_3, pos_4 = mushroom_1.position, mushroom_2.position
-    pos_list_1 = [pos_2, pos_3, pos_4]
-    pos_list_2 = [pos_3, pos_4]
-    while pos_1 in pos_list_1 or pos_2 in pos_list_2 or pos_3 == pos_4:
-        apple.position = apple.randomize_position()
-        mushroom_1.position = mushroom_1.randomize_position()
-        mushroom_2.position = mushroom_2.randomize_position()
-    occupied_positions = pos_list_1
-
+    occupied_positions = start_position(snake, apple, mushroom_1, mushroom_2)
     while True:
         # Присваивание и изменение скорости змейки
-        if snake.length < 15:
-            a = 0
-        elif 15 <= snake.length < 30:
-            a = 5
-        elif 30 <= snake.length < 50:
-            a = 10
-        else:
-            a = 15
-        clock.tick(SPEED + a)
+        speed_tweaking(snake.length, SPEED)
 
         # Отрисовка всех элементов
         pygame.display.update()
         screen.fill(BOARD_BACKGROUND_COLOR)
-        background = pygame.image.load('./images/PixelBackground.png')
+        background = pygame.image.load(BACKGROUND_IMAGE)
         screen.blit(background, (0, 0))
         apple.draw(screen)
         mushroom_1.draw(screen)
@@ -225,37 +269,10 @@ def main():  # noqa: max-complexity=13
         handle_keys(snake)
         snake.update_direction()
 
-        # Обработка события столкновения с яблоком
-        if snake.positions[0] == apple.position:
-            occupied_positions.remove(apple.position)
-            while apple.position in occupied_positions + snake.positions:
-                apple.position = apple.randomize_position()
-            occupied_positions.insert(0, apple.position)
-            snake.length += 1
-
-        # Обработка события столкновения с mushroom_1
-        if snake.positions[0] == mushroom_1.position:
-            occupied_positions.remove(mushroom_1.position)
-            while mushroom_1.position in occupied_positions + snake.positions:
-                mushroom_1.position = mushroom_1.randomize_position()
-            occupied_positions.insert(1, mushroom_1.position)
-            if snake.length > 1:
-                snake.length -= 1
-                snake.positions.pop()
-
-        # Обработка события столкновения с mushroom_2
-        if snake.positions[0] == mushroom_2.position:
-            occupied_positions.remove(mushroom_2.position)
-            while mushroom_2.position in occupied_positions + snake.positions:
-                mushroom_2.position = mushroom_2.randomize_position()
-            occupied_positions.insert(1, mushroom_2.position)
-            if 1 < snake.length < 3:
-                snake.length -= 1
-                snake.positions.pop()
-            elif snake.length >= 3:
-                snake.length -= 2
-                snake.positions.pop()
-                snake.positions.pop()
+        # Обработка событий столкновения с объектами
+        crossing_apple(snake, apple, occupied_positions)
+        crossing_mushroom_1(snake, mushroom_1, occupied_positions)
+        crossing_mushroom_2(snake, mushroom_2, occupied_positions)
 
 
 if __name__ == '__main__':
